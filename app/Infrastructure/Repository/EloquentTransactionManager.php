17233fdcb202bdd6a665fdfef98c539b8c548aab
Sergey <prcelsus@gmail.com>
2024-04-27 17:14:03 +0300

    init

--- app/Infrastructure/Repository/EloquentTransactionManager.php
+++ app/Infrastructure/Repository/EloquentTransactionManager.php
@@ -11,8 +11,6 @@ class EloquentTransactionManager implements TransactionManagerContract
     public function startTransaction($isolationLevel = TransactionManagerContract::ISOLATION_LEVEL_SERIALIZABLE): void
     {
         DB::statement("START TRANSACTION ISOLATION LEVEL $isolationLevel;");
-        DB::statement("SET LOCAL lock_timeout = '5s';");
-        DB::statement("SET LOCAL statement_timeout = '5s';");
     }
 
     public function commitTransaction(): void
